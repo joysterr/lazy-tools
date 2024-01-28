34af187cbcbd7b8d0db089b6fda49feccf86c34a
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-01-28 19:23:49 +0000

    basic auto-organise download folder program

---
+++ auto_organise.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+
+user = os.path.expanduser("~")
+source = f'{user}/Downloads'
+dest_images = f'{source}/Images'
+dest_pdfs = f'{source}/PDF'
+dest_installer = f'{source}/Installers'
+dest_audio = f'{source}/Audio'
+
+destinations_arr = [dest_images, dest_pdfs, dest_installer, dest_audio]
+all_files = os.listdir(source)
+
+
+def check_folders():
+    for dir in destinations_arr:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+            print(f'Created new folder: {dir}')
+        else:
+            continue
+    print("Folder checks complete. OK to proceed.")
+
+
+def move_files():
+    for f in all_files:
+        src_path = os.path.join(source, f)
+        if f.endswith("jpg") or f.endswith("png") or f.endswith("heif") or f.endswith("raw"):
+            dst_path = os.path.join(dest_images, f)
+            shutil.move(src_path, dst_path)
+        elif f.endswith("pdf"):
+            dst_path = os.path.join(dest_pdfs, f)
+            shutil.move(src_path, dst_path)
+        elif f.endswith("dmg") or f.endswith("pkg") or f.endswith("exe"):
+            dst_path = os.path.join(dest_installer, f)
+            shutil.move(src_path, dst_path)
+        elif f.endswith("mp3") or f.endswith("wav") or f.endswith("flac") or f.endswith("alac"):
+            dst_path = os.path.join(dest_audio, f)
+            shutil.move(src_path, dst_path)
+        else:
+            continue
+    print("Downloads Folder is now auto-organised.")
+               
+
+check_folders()
+move_files()
